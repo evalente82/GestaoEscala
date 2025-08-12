a16619a9bda05d89d56735478257287bd654eaeb
Endrigo Valente <endrigo.valente@gmail.com>
2025-08-12 02:15:27 -0300

    Update gerarChave.cs

--- Back/GestaoEscalaPermutas.Server/gerarChave.cs
+++ Back/GestaoEscalaPermutas.Server/gerarChave.cs
@@ -1,16 +1,1 @@
-﻿using System;
-using System.Security.Cryptography;
-using System.Text;
-
-public class gerarChave
-{
-    public void teste()
-    {
-        using var rng = new RNGCryptoServiceProvider();
-        byte[] secretKey = new byte[32]; // 🔹 256 bits (32 bytes)
-        rng.GetBytes(secretKey);
-
-        string chaveBase64 = Convert.ToBase64String(secretKey);
-        Console.WriteLine($"🔑 SUA NOVA CHAVE SECRETA (Base64): {chaveBase64}");
-    }
-}
+usar do bkp
